1e713d5d6b13a7ee088bb4f700f398f7130f5f16
Pao Corrales <paobcorrales@gmail.com>
2021-04-25 17:47:28 -0300

    Cambio nombre y agrego doc get_perfil()

--- R/get-perfil.R
+++ R/get-perfil.R
@@ -1,23 +1,30 @@
 #' Obtiene perfiles de suelo
 #'
-#' @param perfil_ids vector de id numéricos de perfiles de suelo.
-#' @param dir directorio donde descargar los archivos.
+#' \code{get_perfies} descarga perfiles desde SISINTA o lee perfiles
+#' descargados previamente.
+#'
+#' @param perfil_ids vector numérico de ids de perfiles de suelo.
+#' @param dir directorio donde se guardan los datos de perfiles o
+#' donde se leen perfiles ya descargados. Por defecto, los perfiles
+#' descargados se guardan en una carpeta temporal.
+#' @param refresh boleano, fuerza la descarga de los datos para
+#' actualizar los perfiles ya descargados.
 #'
 #' @return
 #' Un data.frame.
 #'
 #' @examples
 #'
-#' sis_perfiles(c(3238, 4634))
+#' get_perfiles(c(3238, 4634))
 #'
 #'
 #' @export
-sis_perfiles <- function(perfil_ids, dir = tempdir()) {
+get_perfiles <- function(perfil_ids, dir = tempdir(), refresh = FALSE) {
   urls <- paste0("http://sisinta.inta.gob.ar/es/perfiles/", perfil_ids, ".csv")
   files <- file.path(dir, paste0("sisinta_", perfil_ids, ".csv"))
 
   data <- lapply(seq_along(urls), function(i) {
-    if (!file.exists(files[i])) {
+    if (!file.exists(files[i]) | refresh == TRUE) {
       download.file(urls[i], files[i])
     }
 
