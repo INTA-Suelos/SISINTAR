99db84e9d446f37e646dcb60b31855d5d4ff30b6
paocorrales <paobcorrales@gmail.com>
2021-05-22 16:49:20 -0300

    Documentación anidar_horizontes()

--- R/anidar-horizontes.R
+++ R/anidar-horizontes.R
@@ -1,16 +1,22 @@
 #' Anida datos de horizontes
 #'
+#' La función \code{anidar_horizontes} permite organizar la información de uno
+#' o más perfiles y sus horizontes en un data.frame. Cada fila se corresponde con
+#' un perfil y los datos de los horizontes se guardan en una columna de data.frames.
+#'
 #' @param perfiles un data.frame con perfiles (salida de [get_perfiles()])
 #'
 #' @returns
-#' Un data.frame anidado. El rsultado tiene una fila por cada perfil y una
+#' Un data.frame anidado. El resultado tiene una fila por cada perfil y una
 #' columna por cada dato del perfil más una columna llamada "horizontes" que
-#' tiene un data.frame con los horizontes de ese perfil.
+#' contiene un data.frame con los horizontes de cada perfil.
 #'
 #' @examples
 #'
 #' perfiles <- get_perfiles(c(3238, 4634))
-#' anidar_horizontes(perfiles)
+#' anidados <- anidar_horizontes(perfiles)
+#' #Para trabajar con horizontes anidados o desanidarlos se puede utlizar la librería {tidyr}
+#' #tidyr::unnest(anidados, cols = c(horizontes))
 #'
 #' @export
 anidar_horizontes <- function(perfiles) {
