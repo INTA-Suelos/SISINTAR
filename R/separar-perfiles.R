944966be2b2994deb591d24981032ce7212e28c7
paocorrales <paobcorrales@gmail.com>
2021-05-22 17:57:25 -0300

    Documentación separar_perfiles()

--- R/separar-perfiles.R
+++ R/separar-perfiles.R
@@ -1,13 +1,18 @@
 #' Separar datos de sitio y horizonte en dos tablas
 #'
+#' Esta función permite separar el data.frame que genera [get_perfiles()] que contiene
+#' la información del sitio y sus horizontes en dos data.frames organizados en una lista.
+#'
 #' @param perfiles un data.frame con perfiles (salida de [get_perfiles()])
 #'
 #' @returns
-#' Una lista con dos elementos. El elemento `sitios`, es un data.frmae con
+#' Una lista con dos elementos. El elemento `sitios`, es un data.frame con
 #' la información de los sitios y el elemneto `horizontes` es un data.frame
 #' con la información de horizontes.
 #'
-#'
+#' @examples
+#' perfiles <- get_perfiles(c(3238))
+#' separar_perfiles(perfiles)
 #'
 #' @export
 separar_perfiles <- function(perfiles) {
