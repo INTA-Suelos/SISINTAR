8e51bce3493625f4b3023c65725b8bcf427e1593
Elio Campitelli <eliocampitelli@gmail.com>
2022-02-10 15:54:22 -0300

    Agrega test de desanidar

--- tests/testthat/test-anidar_horizontes.R
+++ tests/testthat/test-anidar_horizontes.R
@@ -1,6 +1,16 @@
+data(perfiles)
 test_that("anida", {
-  expect_error(anidado <- anidar_horizontes(perfiles), NA)
+  expect_error(anidado <<- anidar_horizontes(perfiles), NA)
   expect_equal(nrow(anidado), 3)
   expect_true(!is.null(anidado$horizontes))
   expect_s3_class(anidado$horizontes[[1]], "data.frame")
 })
+
+test_that("desanidar recupera el original", {
+
+  desanidado <- desanidar_horizontes(anidado)
+  expect_identical(perfiles[colnames(desanidado)], desanidado)
+})
+
+
+
